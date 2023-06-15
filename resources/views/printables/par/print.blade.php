062f254eed3cfe500e800ab2bffcf350a3685619
Rene Hernaez Empremiado <empremiado.rene11@gmail.com>
2023-06-15 08:59:29 +0800

    Updates

--- resources/views/printables/par/print.blade.php
+++ resources/views/printables/par/print.blade.php
@@ -1,23 +1,29 @@
-@php
-    use SimpleSoftwareIO\QrCode\Facades\QrCode;
-@endphp
-
 @extends('printables.print_layouts.print_layout_main')
 
 @section('styles')
 @endsection
 
 @section('wrapper')
-    <div class="printable">
+    <style>
+        @font-face {
+            font-family: 'Cambria';
+            src: url({{ storage_path("fonts/cambria.ttf") }}) format("truetype");
+            font-weight: 700;
+            font-style: normal;
+        }
+        .page-breaks {
+            page-break-after: always;
+        }
+    </style>
+    <div class="printable" style="font-family: Cambria,Arial">
 
         <div style="width: 100%;">
-            <div class="" style="margin-bottom: 100px; padding-top: 10px;">
+            <div class="" style="margin-bottom: 100px; padding-top: 10px; ">
                 <div>
                     <img src="{{ asset('images/sra.png') }}" style="width:100px; float: left">
                 </div>
                 <div style="float: left; text-align: left; margin-left: 15px">
                     <p class="no-margin" style="font-size: 14px; margin-bottom: -4px">Republic of the Philippines</p>
-                    <p class="no-margin" style="font-size: 14px; margin-bottom: -4px">Department of Agriculture</p>
                     <p class="no-margin text-strong" style="font-size: 14px; margin-bottom: -4px">SUGAR REGULATORY ADMINISTRATION</p>
                     <p class="no-margin" style="font-size: 14px; margin-bottom: -4px">Araneta St., Singcang, Bacolod City, Tel No. 433-6891</p>
                     <p style="font-size: 20px;"><strong>PROPERTY ACKNOWLEDGEMENT RECEIPT</strong></p>
