ac92ca6897910d25649010fa48d91ba5d9c5e23f
MISVIS <sramisvis@gmail.com>
2023-08-23 17:43:01 +0800

    print property tag by location

--- resources/views/ppu/par/index.blade.php
+++ resources/views/ppu/par/index.blade.php
@@ -56,7 +56,7 @@
           <form id="property-tag-by-location-form">
               <div class="modal-header">
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                  <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                  <h4 class="modal-title" id="myModalLabel">Tag by location</h4>
               </div>
               <div class="modal-body">
                   <div class="row">
