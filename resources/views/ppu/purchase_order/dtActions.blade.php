1af6d0668b81e2861eb52de1a0ec57533b8f3880
Rene Hernaez Empremiado <empremiado.rene11@gmail.com>
2023-09-06 13:38:30 +0800

    Updates

--- resources/views/ppu/purchase_order/dtActions.blade.php
+++ resources/views/ppu/purchase_order/dtActions.blade.php
@@ -3,7 +3,7 @@
     <a class="btn btn-default btn-sm edit_btn" href="{{route('dashboard.po.edit',$data->slug)}}">
         <i class="fa fa-edit"></i>
     </a>
-    <a class="btn btn-default btn-sm" href="{{route('dashboard.po.print1',$data->slug)}}" target="_blank">
+    <a class="btn btn-default btn-sm" href="{{route('dashboard.po.print',$data->slug)}}" target="_blank">
         <i class="fa fa-print"></i>
     </a>
 </div>
