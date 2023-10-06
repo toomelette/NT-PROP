6d1a58a3d39ceb96b3a63d27b5761737842b7d6f
MISVIS <sramisvis@gmail.com>
2023-10-06 14:26:29 +0800

    pr timeline

---
+++ app/Swep/Traits/PRTimelineTrait.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Swep\Traits;
+
+trait PRTimelineTrait
+{
+
+}
---
+++ resources/views/ppu/pr_my/show.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.modal-content')
+
+@section('modal-header')
+
+@endsection
+
+@section('modal-body')
+
+  
+@endsection
+
+@section('modal-footer')
+    
+@endsection
+
+@section('scripts')
+<script type="text/javascript">
+   
+</script>
+@endsection
+
