e2f9acba449b2192cdd82a8f29cecba53427695c
Rene Hernaez Empremiado <empremiado.rene11@gmail.com>
2023-10-12 09:22:28 +0800

    Updates

--- resources/views/ppu/ics/create.blade.php
+++ resources/views/ppu/ics/create.blade.php
@@ -236,7 +236,7 @@
             let parentTrId = t.parents('tr').attr('id');
             let data = e.params.data;
 
-            $("#"+parentTrId+" [for='stockNo']").val(data.id);
+            $("#"+parentTrId+" [for='stock_no']").val(data.id);
             $("#"+parentTrId+" [for='uom']").val(data.populate.uom);
             $("#"+parentTrId+" [for='itemName']").val(data.text);
         });
