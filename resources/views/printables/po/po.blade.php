882c9196a62dc5e41c30c10ad2d50ba617c7807e
Rene Hernaez Empremiado <empremiado.rene11@gmail.com>
2024-02-20 09:14:09 +0800

    Updates

--- resources/views/printables/po/po.blade.php
+++ resources/views/printables/po/po.blade.php
@@ -411,7 +411,7 @@
             </td>
         </tr>
     </table>
-    <div class="qms-right">
+    <div class="qms-right" style="font-size: 12px;">
         <p class="no-margin">FM-AFD-PPS-003,Rev.00</p>
         <p class="no-margin">Effectivity Date: March 12, 2015</p>
     </div>
