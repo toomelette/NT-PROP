41d373fdea610f7221fcbd086c0e2079e0c29136
Darryl <darrylvanbasa@gmail.com>
2024-02-29 13:43:00 +0800

    Request for Vehicle Update

--- resources/views/ppu/request_vehicle/myRequestsDtActions.blade.php
+++ resources/views/ppu/request_vehicle/myRequestsDtActions.blade.php
@@ -1,5 +1,5 @@
 <div class="btn-group">
-{{--    @if($data->action=="APPROVED")--}}
+    @if($data->action=="APPROVED")
         <a href="{{route('dashboard.request_vehicle.print_own',$data->slug)}}" target="_blank" data="{{$data->slug}}" type="button" class="btn btn-default btn-sm"><i class="fa fa-print"></i></a>
-{{--    @endif--}}
+    @endif
 </div>
