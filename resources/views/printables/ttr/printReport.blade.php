cab31c5522bb6850dd76cc6f659cd13a279135c7
Darryl <darrylvanbasa@gmail.com>
2024-04-04 14:12:30 +0800

    update

--- resources/views/printables/ttr/printReport.blade.php
+++ resources/views/printables/ttr/printReport.blade.php
@@ -96,9 +96,10 @@
                             <td style="text-align: center; width: 7%" >{{$a = $tripTicket->distance_traveled}}</td>
                             <td style="text-align: center; width: 7%" >{{$b = $tripTicket->consumed}}</td>
                             <td style="text-align: center; width: 7%" >
-                                @if($tripTicket->consumed != null or $tripTicket->consumed != 0)
-                                    {{$tripTicket->consumed}}
-{{--                                {{number_format($tripTicket->distance_traveled / $tripTicket->consumed, 2) }}--}}
+                                @if($tripTicket->consumed != null or $tripTicket->consumed != 0 )
+                                    @if($triptikcket->distance_traveled != null or $triptikcket->distance_traveled != 0 )
+                                        {{number_format($tripTicket->distance_traveled / $tripTicket->consumed, 2) }}
+                                    @endif
                                 @endif
                             </td>
                             <td style="text-align: center; width: 7%" >{{$d = $vehicle->normal_usage}}</td>
